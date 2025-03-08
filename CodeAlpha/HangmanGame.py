d43bb309f305985d1af74562fb987b5bac138647
Yousaf Afzal <jiyamalik360@gmail.com>
2025-03-08 15:43:37 +0500

    Added HangmanGame.py in CodeAlpha

---
+++ CodeAlpha/HangmanGame.py
@@ -0,0 +1,47 @@
+import random
+
+
+def choose_word():
+    words = ['python', 'romaisa' 'meet', 'developer', 'hangman', 'function', 'variable', 'algorithm', 'computer']
+    return random.choice(words)
+
+
+def display_word(word, guessed_letters):
+    return ' '.join(letter if letter in guessed_letters else '_' for letter in word)
+
+
+def hangman():
+    word = choose_word()
+    guessed_letters = set()
+    attempts = 6
+
+    print("Welcome to Hangman!")
+
+    while attempts > 0:
+        print("\n" + display_word(word, guessed_letters))
+        print(f"Attempts left: {attempts}")
+        guess = input("Guess a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid input. Please enter a single letter.")
+            continue
+
+        if guess in guessed_letters:
+            print("You've already guessed that letter.")
+            continue
+
+        guessed_letters.add(guess)
+
+        if guess not in word:
+            attempts -= 1
+            print("Incorrect guess!")
+
+        if all(letter in guessed_letters for letter in word):
+            print("\nCongratulations! You guessed the word:", word)
+            break
+    else:
+        print("\nGame over! The word was:", word)
+
+
+if __name__ == "__main__":
+    hangman()
